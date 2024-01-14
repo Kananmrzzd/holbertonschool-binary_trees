97cf3eca4c5333fb4d73c110546803b8300df16d
dArk10R4 <goylerov04@gmail.com>
2024-01-14 05:28:48 -0800

    1

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -14,7 +14,7 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 	if (!parent)
 		return (NULL);
 
-	(*parent)->left = binary_tree_node(parent, value);
+	parent->left = binary_tree_node(parent, value);
 
-	return ((*parent)->left);
+	return (parent->left);
 }
