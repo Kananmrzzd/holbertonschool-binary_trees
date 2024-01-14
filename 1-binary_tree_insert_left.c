1430504ea092b7a63a25ff347ee2f5bd9864e24e
dArk10R4 <goylerov04@gmail.com>
2024-01-14 05:27:44 -0800

    1

---
+++ 1-binary_tree_insert_left.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include "binary_trees.h"
+
+/**
+ * binary_tree_insert_left - inserts to left
+ * @parent: parent
+ * @value: value of new node
+ *
+ * Return: new node
+ */
+
+binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
+{
+	if (!parent)
+		return (NULL);
+
+	(*parent)->left = binary_tree_node(parent, value);
+
+	return ((*parent)->left);
+}
