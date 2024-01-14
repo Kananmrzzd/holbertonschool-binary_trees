e1f3d6ea2e6ac148f1d02551d106d9152337732e
dArk10R4 <goylerov04@gmail.com>
2024-01-14 06:47:55 -0800

    5

---
+++ 5-binary_tree_is_root.c
@@ -0,0 +1,12 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_is_root - is root
+ * @node: node
+ *
+ * Return: 1 or 0
+ */
+int binary_tree_is_root(const binary_tree_t *node)
+{
+	return (root && !root->parent);
+}
