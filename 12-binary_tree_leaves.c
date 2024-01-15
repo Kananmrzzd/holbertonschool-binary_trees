151c27178340b4606ea4a99a7156cfb3b98eb7e5
Kananmrzzd <kananmirzazada@gmail.com>
2024-01-14 22:19:02 -0800

    a

--- 12-binary_tree_leaves.c
+++ 12-binary_tree_leaves.c
@@ -6,16 +6,17 @@
  *
  * Return: tree size
  */
-size_t binary_tree_size(const binary_tree_t *tree)
+size_t binary_tree_leaves(const binary_tree_t *tree)
 {
 	int l = 0;
 
 	if (tree)
 	{
-		if (tree->right)
+		if (tree->right && tree->left)
 			l += 1;
-		if (tree->left)
-			l += 1;
+
+		binary_tree_leaves(tree->right);
+		binary_tree_leaves(tree->left);
 	}
 
 	return (l);
---
+++ 13-binary_tree_nodes.c
@@ -0,0 +1,21 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_nodes - counts nodes with at least 1 child
+ * @tree: pointer to the root node of the tree
+ *
+ * Return: count
+ */
+size_t binary_tree_nodes(const binary_tree_t *tree)
+{
+	int cnt = 0;
+
+	if (tree && (tree->right || tree->left))
+	{
+		cnt += 1;
+		cnt += binary_tree_size(tree->left);
+		cnt += binary_tree_size(tree->right);
+	}
+
+	return (s);
+}
